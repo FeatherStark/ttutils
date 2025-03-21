079a96b3d7c392e0b800a8d10cafb2ce059d7dad
FeatherStark <featherstark@outlook.com>
2025-03-21 09:58:03 +0800

    Slice

---
+++ slice.go
@@ -0,0 +1,16 @@
+package ttutils
+
+// SliceRemoveDuplicates 切片去重
+// Args: slice []string 切片
+// Returns: []string 去重后的切片
+func SliceRemoveDuplicates(slice []string) []string {
+	uniqueMap := make(map[string]bool)
+	var result []string
+	for _, item := range slice {
+		if _, exists := uniqueMap[item]; !exists {
+			uniqueMap[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
+}
